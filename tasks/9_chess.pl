5ab50466eb17ec1dd42f709b470ce7f2621996d5
Gleb Karpushkin <rugleb@gmail.com>
2018-11-06 18:22:57 +0300

    created move predicate

--- tasks/9_chess.pl
+++ tasks/9_chess.pl
@@ -1,11 +1,18 @@
 % Написать программу отыскания кратчайшего пути коня из позиции a1
 % на стандартной шахматной доске в произвольную позицию.
 
-possibleMove(X, Y, NewX, NewY) :- NewX is I+1, NewY is Y+2.
-possibleMove(X, Y, NewX, NewY) :- NewX is I+2, NewY is Y+1.
-possibleMove(X, Y, NewX, NewY) :- NewX is I+2, NewY is Y-1.
-possibleMove(X, Y, NewX, NewY) :- NewX is I+1, NewY is Y-2.
-possibleMove(X, Y, NewX, NewY) :- NewX is I-1, NewY is Y-2.
-possibleMove(X, Y, NewX, NewY) :- NewX is I-2, NewY is Y+1.
-possibleMove(X, Y, NewX, NewY) :- NewX is I-2, NewY is Y-1.
-possibleMove(X, Y, NewX, NewY) :- NewX is I-1, NewY is Y+2.
+possibleMove(X, Y, NewX, NewY) :- NewX is X+1, NewY is Y+2.
+possibleMove(X, Y, NewX, NewY) :- NewX is X+2, NewY is Y+1.
+possibleMove(X, Y, NewX, NewY) :- NewX is X+2, NewY is Y-1.
+possibleMove(X, Y, NewX, NewY) :- NewX is X+1, NewY is Y-2.
+possibleMove(X, Y, NewX, NewY) :- NewX is X-1, NewY is Y-2.
+possibleMove(X, Y, NewX, NewY) :- NewX is X-2, NewY is Y+1.
+possibleMove(X, Y, NewX, NewY) :- NewX is X-2, NewY is Y-1.
+possibleMove(X, Y, NewX, NewY) :- NewX is X-1, NewY is Y+2.
+
+move(X, Y, NewX, NewY) :-
+    possibleMove(X, Y, NewX, NewY),
+    NewX >= 0,
+    NewY >= 0,
+    NewX =< 7,
+    NewY =< 7.
