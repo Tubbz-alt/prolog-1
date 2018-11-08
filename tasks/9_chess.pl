bdf15e6ad46c558c786b08a70175ff9f1983d6d1
Gleb Karpushkin <rugleb@gmail.com>
2018-11-08 16:58:37 +0300

    fixed task 9

--- tasks/9_chess.pl
+++ tasks/9_chess.pl
@@ -1,6 +1,15 @@
 % Написать программу отыскания кратчайшего пути коня из позиции a1
 % на стандартной шахматной доске в произвольную позицию.
 
+min(X, Y, X) :- X =< Y.
+min(X, Y, Y) :- Y =< X.
+
+sublist([H|_], Len, H) :- length(H, Len).
+sublist([_|T], Len, List) :- sublist(T, Len, List).
+
+member(X, [Head|_]) :- X = Head, !.
+member(X, [_|Tail]) :- member(X, Tail).
+
 possibleMove(X, Y, NewX, NewY) :- NewX is X+1, NewY is Y+2.
 possibleMove(X, Y, NewX, NewY) :- NewX is X+2, NewY is Y+1.
 possibleMove(X, Y, NewX, NewY) :- NewX is X+2, NewY is Y-1.
@@ -13,22 +22,35 @@ possibleMove(X, Y, NewX, NewY) :- NewX is X-1, NewY is Y+2.
 isValidX(Value) :- Value >= 0, Value =< 7.
 isValidY(Value) :- Value >= 0, Value =< 7.
 
-move(X, Y, NewX, NewY) :-
+point(X, Y, [X, Y]) :- !.
+
+move(From, To) :-
+    point(X, Y, From),
     possibleMove(X, Y, NewX, NewY),
     isValidX(NewX),
-    isValidY(NewY).
+    isValidY(NewY),
+    point(NewX, NewY, To).
 
-path(X, Y, X, Y, Path) :- Path = [[X, Y], [X, Y]], !.
-
-path(X, Y, ToX, ToY, Path) :-
-    move(X, Y, TmpX, TmpY),
-    TmpX = ToX,
-    TmpY = ToY,
-    Path = [[X, Y], [ToX, ToY]],
+path(From, To, PrevPath, Path) :-
+    move(From, To),
+    append(PrevPath, [To], Path),
     !.
 
-path(X, Y, ToX, ToY, Path) :-
-    move(X, Y, TmpX, TmpY),
-    From = [X, Y],
-    path(TmpX, TmpY, ToX, ToY, Path2),
-    append([From], Path2, Path).
+path(From, To, PrevPath, Path) :-
+    move(From, Tmp),
+    not(member(Tmp, PrevPath)),
+    append(PrevPath, [Tmp], TmpPath),
+    !,
+    path(Tmp, To, TmpPath, Path).
+
+way(To, Way) :- path([0, 0], To, [], Way).
+
+shortway(To) :-
+    findall(Way, way(To, Way), Ways),
+    shortest(Ways, Shortest),
+    print(Shortest).
+
+minlen([], 999).
+minlen([H|T], Min) :- minlen(T, TMin), length(H, HMin), min(TMin, HMin, Min), !.
+
+shortest(X, Shortest) :- minlen(X, Len), sublist(X, Len, Shortest).
